39552e4cdf3c92672e60877d519dadbceae897d4
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-09-09 22:35:11 -0300

    feat: add attributes to Account class

--- src/main/java/dev/dreamer/bank/domain/Account.java
+++ src/main/java/dev/dreamer/bank/domain/Account.java
@@ -1,4 +1,17 @@
 package dev.dreamer.bank.domain;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Account {
+
+    UUID id;
+    String accountNumber;
+    UUID userId;
+    BigDecimal balance;
+    AccountType accountType;
+    AccountStatus status;
+    LocalDateTime creationDate;
+
 }
