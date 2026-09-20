8278d27b4b7e4ed6e76c539816f3d89e2bb9a027
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-09-19 23:29:44 -0300

    feat: add account create validation

--- src/main/java/dev/dreamer/bank/domain/Account.java
+++ src/main/java/dev/dreamer/bank/domain/Account.java
@@ -2,6 +2,7 @@ package dev.dreamer.bank.domain;
 
 import dev.dreamer.bank.domain.enums.AccountStatus;
 import dev.dreamer.bank.domain.enums.AccountType;
+import dev.dreamer.bank.domain.exceptions.InvalidAccountDataException;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -20,9 +21,66 @@ public class Account {
     public Account() {
     }
 
-    public static Account create( String accountNumber, UUID userId, BigDecimal balance, AccountType accountType, AccountStatus accountStatus) {
-        return null;
+    public static Account create( String accountNumber, UUID userId, BigDecimal balance, AccountType accountType, AccountStatus accountStatus,  LocalDateTime creationDate ) {
+        validate(accountNumber, userId, balance, accountType, accountStatus, creationDate);
+
+       return null;
     }
 
 
+    private static void validate(String accountNumber, UUID userId, BigDecimal balance, AccountType accountType, AccountStatus accountStatus,   LocalDateTime creationDate  ) {
+        if (accountNumber == null|| accountNumber.isBlank()){
+            throw new InvalidAccountDataException("Account number cannot be null or empty");
+        }
+        if (accountType == null){
+            throw new InvalidAccountDataException("Account type cannot be null");
+        }
+        if (userId == null){
+            throw new InvalidAccountDataException("Account user cannot be null");
+        }
+        if (balance == null){
+            throw new InvalidAccountDataException("Balance cannot be null");
+        }
+        if (balance.compareTo(BigDecimal.ZERO) < 0){
+            throw new InvalidAccountDataException("Balance cannot be less than zero");
+        }
+        if (accountStatus == null){
+            throw new InvalidAccountDataException("Account status cannot be null");
+        }
+        if (creationDate == null){
+            throw new InvalidAccountDataException("Account creation date cannot be null");
+        }
+
+
+    }
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
 }
