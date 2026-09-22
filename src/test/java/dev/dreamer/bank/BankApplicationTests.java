2bd208c8606afb4daf87d3c08eac1aa871d106f5
Matheus Alves <matheus.adeoliveira11@gmail.com>
2026-09-22 15:06:58 -0300

    chore: setup Testcontainers
    related issue: #23

--- src/test/java/dev/dreamer/bank/BankApplicationTests.java
+++ src/test/java/dev/dreamer/bank/BankApplicationTests.java
@@ -1,9 +1,13 @@
 package dev.dreamer.bank;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(TestcontainersConfiguration.class)
 class BankApplicationTests {
 
 	@Test
