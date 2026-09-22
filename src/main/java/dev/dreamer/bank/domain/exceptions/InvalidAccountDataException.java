28aabc39596e9e7ed7c15801558a97defd125e94
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-09-21 21:56:21 -0300

    refact: add account exception in account's exception folder

--- src/main/java/dev/dreamer/bank/domain/exceptions/InvalidAccountDataException.java
+++
@@ -1,7 +0,0 @@
-package dev.dreamer.bank.domain.exceptions;
-
-public class InvalidAccountDataException extends RuntimeException {
-    public InvalidAccountDataException(String message) {
-        super(message);
-    }
-}
