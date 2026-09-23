2c090486bad3f719828358d327d755d72cd108e2
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2026-09-22 21:48:03 -0300

    feat: add InvalidAmountException class

--- src/main/java/dev/dreamer/bank/domain/exceptions/account/InvalidAmountException.java
+++ src/main/java/dev/dreamer/bank/domain/exceptions/account/InvalidAmountException.java
@@ -1,7 +1,7 @@
 package dev.dreamer.bank.domain.exceptions.account;
 
 public class InvalidAmountException extends RuntimeException {
-  public InvalidAmountException(String message) {
-    super(message);
-  }
+    public InvalidAmountException(String message) {
+        super(message);
+    }
 }
